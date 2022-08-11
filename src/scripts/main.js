9f98d7facd6f038d131b77f63da971c772f72ca8
Web Monkey <104577087+zhuk1707@users.noreply.github.com>
2022-08-11 22:55:08 +0300

    added gradient generator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,31 @@
-let background = document.body
-
 const hex = [
   1, 2, 3, 4, 5, 6, 7, 8,
   9, 'a', 'b', 'c', 'd' , 'e', 'f'
 ]
 
+const background = document.body
+// let logoHover = document.querySelector('.header__logo')
+const generateGradientBtn = document.querySelector('.panel__button_generate')
+
+const firstPanelHex = document.querySelector('.panel__color_first .panel__hex')
+const secondPanelHex =   document.querySelector('.panel__color_second' +
+  ' .panel__hex')
+const firstPanelPreview = document.querySelector('.panel__color_first' +
+  ' .panel__preview')
+const secondPanelPreview = document.querySelector('.panel__color_second' +
+  ' .panel__preview')
+
+const copyBtn = document.querySelector('.panel__button_copy')
+
 function generateGradient() {
-  let generatedColor1 = '#' + generateHex()
-  let generatedColor2 = '#' + generateHex()
+  const generatedColor1 = '#' + generateHex()
+  const generatedColor2 = '#' + generateHex()
+  
   background.style.background = `linear-gradient(90deg,  ${generatedColor1}, ${generatedColor2})`
+  firstPanelHex.innerHTML = generatedColor1.toUpperCase()
+  secondPanelHex.innerHTML = generatedColor2.toUpperCase()
+  firstPanelPreview.style.background = generatedColor1
+  secondPanelPreview.style.background = generatedColor2
 }
 
 function generateHex() {
@@ -19,3 +36,12 @@ function generateHex() {
   }
   return result
 }
+
+function copyCSS() {
+  
+}
+
+generateGradientBtn.addEventListener('click', generateGradient)
+generateGradientBtn.addEventListener('click', copyCSS)
+
+generateGradient()
